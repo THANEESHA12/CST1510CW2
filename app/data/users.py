6e59b828da62744263f7af9a2612b943f9c1858c
Thaneesha <lowtooshaina@gmail.com>
2025-12-11 02:52:30 +0400

    Crud operations for user

--- app/data/users.py
+++ app/data/users.py
@@ -1,25 +1,29 @@
-from app.data.db import connect_database
+from app.data.db import connect_database #From db the function connect_database is being imported
 
+#This function is being defined to get user by their username
 def get_user_by_username(username):
     """Retrieve user by username."""
-    conn = connect_database()
-    cursor = conn.cursor()
+    conn = connect_database() #Opening a connection to the database
+    cursor = conn.cursor() #A cursor is created to run SQL command
+    #The cursor executes the SQL command in which a query is selected from the users table
     cursor.execute(
         "SELECT * FROM users WHERE username = ?",
         (username,)
     )
-    user = cursor.fetchone()
-    conn.close()
+    user = cursor.fetchone() #This fetches the first matched row or none if the user is not found
+    conn.close() #Clsoing the connection to the database
     return user
 
+#This function is being defined to insert new users
 def insert_user(username, password_hash, role='user'):
     """Insert new user."""
-    conn = connect_database()
-    cursor = conn.cursor()
+    conn = connect_database() #Opening a connection to the database
+    cursor = conn.cursor() #A cursor is created to run the SQL command
+    #The cursor is executing the SQL command
     cursor.execute(
         "INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)",
         (username, password_hash, role)
     )
-    conn.commit()
-    conn.close()
-    
+    conn.commit() #If new user is inserted, this will save the changes in the database
+    conn.close() #Closing the connection to the database
+     
