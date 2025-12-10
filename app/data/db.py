d0fca43dc2e036cfc55c004934b5567b685e27f5
Thaneesha <lowtooshaina@gmail.com>
2025-12-11 00:17:24 +0400

    Connection of the databases

--- app/data/db.py
+++ app/data/db.py
@@ -1,25 +1,27 @@
-import sqlite3
-from pathlib import Path
+import sqlite3 #This works with the SQLite database
+from pathlib import Path #This ensures handling file paths
 
+#This is printing where the file is currently at that is its file location
 print(f"[DEBUG] Current file location: {Path(__file__).absolute()}")
 
-# Step 1: Decide where the database file will live
-# We'll keep it inside a folder called DATA, next to this project
+#Going three levels up from the file location and defining the project root's folder - This is kept in the DATA folder
 PROJECT_ROOT = Path(__file__).parent.parent.parent 
 print(f"[DEBUG] Project root: {PROJECT_ROOT.absolute()}")
 
+#Creating a path for the folder DATA inside the project root and this is where the databases is stored at
 DB_FOLDER = PROJECT_ROOT / "DATA"
 print(f"[DEBUG] DB_FOLDER path: {DB_FOLDER.absolute()}")
 
-
-DB_FOLDER.mkdir(exist_ok=True)  # make sure the folder exists
+#Ensuring that the DATA folder exists , exist_ok is preventing errors if the folder is there already
+DB_FOLDER.mkdir(exist_ok=True)  
 print(f"[DEBUG] DB_FOLDER created/exists: {DB_FOLDER.exists()}")
 
+#Defining a full path for intelligence_platform.db and printing to show where the database is created at
 DB_PATH = DB_FOLDER / "intelligence_platform.db"
 print(f"[DEBUG] DB_PATH: {DB_PATH.absolute()}")
 print(f"[DEBUG] DB will be created at: {DB_PATH.absolute()}\n")
 
-# Step 2: Simple function to connect
+# This function will connect the database and if not exists it will automatically be created
 def connect_database(db_path=DB_PATH):
     """
     Connect to the SQLite database.
